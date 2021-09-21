c11dd5d79c32457d56eb55892e5b948be875da9f
Gean Ferreira <gean_roberto@hotmail.com>
2021-09-21 17:09:07 -0300

    função GET tendo como parâmetro o id da url (useRoute)

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,16 +1,29 @@
 <template>
   <h1>Fotos</h1>
+  <div v-for="(photos, index) in photosAlbum" :key="index">
+    <img :src="photos.thumbnailUrl">
+    {{ photos.title }}
+  </div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { useRoute } from "vue-router";
+import { onMounted, ref } from 'vue';
+import { getPhotos } from '../service/gallery-service';
+
 export default {
   setup(){
+    const route = useRoute();
+    const photosAlbum = ref(null)
+
     onMounted(() => {
+      getPhotos(route.params.id).then(resp => {
+        photosAlbum.value = resp
+        console.log(resp)
+      })
+    })
 
-    });
-
-    return {  };
+    return { photosAlbum };
   }
 }
 </script>
--- src/service/gallery-service.js
+++ src/service/gallery-service.js
@@ -1,9 +1,16 @@
 import axios from "axios";
-export { getAlbumsList };
+export { getAlbumsList, getPhotos };
 
 const getAlbumsList = async () => {
   const resp = await axios.get(
     "https://jsonplaceholder.typicode.com/users/1/albums"
+  );
+  return resp.data;
+};
+
+const getPhotos = async (id) => {
+  const resp = await axios.get(
+    `https://jsonplaceholder.typicode.com/albums/${id}/photos`
   );
   return resp.data;
 };
